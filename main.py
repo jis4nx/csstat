e3ee1bfe2fe09948d3d9f24052a8783eef229fe8
Rakibul Islam <74461588+jis4nx@users.noreply.github.com>
2024-05-16 22:36:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from models import Player
 from tasks import parse_player_data
 from cslog import logger
 import uvicorn
-import time
 from fake_useragent import UserAgent
+import asyncio
 
 app = FastAPI()
 URL = "https://csstats.gg/player"
@@ -27,7 +27,7 @@ async def fetch_player_data(player_id: str, retries=5) -> Player:
                 return data
             elif attempt < retries:
                 logger.warning(f"Attempt {attempt + 1} failed with status code {resp.status_code}, retrying...")
-                time.sleep(1)
+                asyncio.sleep(2)
                 
         logger.error(f"Status Code: {resp.status_code}")
         raise HTTPException(status_code=resp.status_code,
